d7d3efc22d8c365b13ef7e35ec6e34d7adf2f286
Sven <SvePu@users.noreply.github.com>
2023-01-04 22:24:21 +0100

    Update profilevisitors.php

--- UPLOAD/inc/plugins/profilevisitors.php
+++ UPLOAD/inc/plugins/profilevisitors.php
@@ -69,7 +69,7 @@ function profilevisitors_info()
         $info_desc .= "<span style=\"font-size: 0.9em;\">(~<a href=\"index.php?module=config-settings&action=change&gid=" . $settings_group['gid'] . "\"> " . $db->escape_string($lang->setting_group_profilevisitors) . " </a>~)</span>";
     }
 
-    if (is_array($plugins_cache) && is_array($plugins_cache['active']) && array_key_exists('profilevisitors', $plugins_cache['active']))
+    if (!empty($plugins_cache) && is_array($plugins_cache) && is_array($plugins_cache['active']) && isset($plugins_cache['active']['profilevisitors']))
     {
         $info_desc .= '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="float: right;" target="_blank" />
 <input type="hidden" name="cmd" value="_s-xclick" />
